906673b497a78019ad1dc9c89b6bc6736572a0a7
Nim <nm-ch@hotmail.com>
2016-11-01 17:52:22 -0700

    added exit animations

--- PresentationLayer/Scripts/calendar.js
+++ PresentationLayer/Scripts/calendar.js
@@ -1,4 +1,9 @@
 ﻿function addActiveClass(element) {
     element.classList.toggle("active");
-    $(".reservation-popup").show(300);
+    $(".reservation-popup").show(250);
 }
+$(".btn-danger").click(function () {
+    $(".reservation-popup").hide(250);
+
+
+});
