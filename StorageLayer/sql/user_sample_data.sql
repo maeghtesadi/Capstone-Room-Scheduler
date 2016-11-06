46163f2aec55bf7d54a1db732377528c63210f5d
limphilip <lim-philip@hotmail.com>
2016-11-06 18:07:11 -0500

    SQL sample data
    Contributors:
    Hannah Ortiz
    Philip Lim
    Ideawin Koun

---
+++ StorageLayer/sql/user_sample_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` VALUES(1, 'Hannah Ortiz', 0, PASSWORD('h_ortiz'));
+INSERT INTO `user` VALUES(2, 'Ideawin Koun', 4, PASSWORD('i_koun'));
+INSERT INTO `user` VALUES(3, 'Philip Lim', 3, PASSWORD('p_lim'));
