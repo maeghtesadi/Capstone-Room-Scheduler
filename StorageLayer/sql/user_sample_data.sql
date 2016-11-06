5cac8ba76a6b83872e4bd86fceac550efeed2961
limphilip <lim-philip@hotmail.com>
2016-11-06 18:22:04 -0500

    wrong sample

--- StorageLayer/sql/user_sample_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `user` VALUES(1, 'Hannah Ortiz', 0, PASSWORD('h_ortiz'));
-INSERT INTO `user` VALUES(2, 'Ideawin Koun', 4, PASSWORD('i_koun'));
-INSERT INTO `user` VALUES(3, 'Philip Lim', 3, PASSWORD('p_lim'));
--- StorageLayer/sql/user_sampledata.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('01','Hannah Ortiz','0',password('hortiz'));
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('02','Ideawin Koun','4',password('ikoun'));
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('03','Philip Lim','2',password('plim'));
