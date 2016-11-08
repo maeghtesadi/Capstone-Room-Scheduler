985ec9034f4c339525365e394f14e7bc2fed7b1f
maeghtesadi <ma_eghtesadi@yahoo.com>
2016-11-08 18:21:20 -0500

    timeslot selecting done by Ali (teamleader)

--- PresentationLayer/Scripts/calendar.js
+++ PresentationLayer/Scripts/calendar.js
@@ -1,11 +1,14 @@
 ﻿var funcCalled = false;
+var seconfuncCalled = false;
+var firstAndLastTimeslot = [0,0];
 var confirmRservation = $(".reservation-popup h1").html();
 //Function is run when any of the timeslot li is clicked
 $(".timeslots li ul li").click(function () {
     if (funcCalled == false) {
         var thisElement = this;
-        var room = $(this).attr('data-room');
-        var timeslot = $(this).attr('data-timeslot');
+        var room = $(this).data("room");
+        var timeslot = $(this).data("timeslot");
+        firstAndLastTimeslot[0] = timeslot;
         funcCalled = true;
         this.classList.toggle("active");
         $(".reservation-popup-test h1").html("Select another timeslot");
@@ -17,17 +20,62 @@ $(".timeslots li ul li").click(function () {
             collision:"fit"
         
         });
-       
+        
         $(".timeslots li ul li").click(function () {
+            var timeslot2 = $(this).data("timeslot");
+
+            if (seconfuncCalled == false) {
+                firstAndLastTimeslot[1] = $(this).data("timeslot") - 1;
+                seconfuncCalled = true;
+            }
+            //if timeslot selected at the begining or after the range
             if ($(this).attr('data-room') == room) {
-                for (var i = parseInt(timeslot) + 1; i <= parseInt($(this).attr('data-timeslot')) ; i++) {
-                    //You need to make find a jquery selector that selects an elemtn with [data-timeslot]=i and [data-room]=room
-                    $("li[data-timeslot='" + i + "']li[data-room='" + room + "']").toggleClass("active");
-                   
+                if (firstAndLastTimeslot[1] < timeslot2) {
+                    for (var i = parseInt(timeslot) + 1; i <= parseInt($(this).attr('data-timeslot')) ; i++) {
+                    
+                        //adds more timeslots to the already active tismeslots
+                        if ($("li[data-timeslot='" + i + "']li[data-room='" + room + "']").hasClass("active")) { }
+                        else {
+                            //toggles active the desired timesots
+                            $("li[data-timeslot='" + i + "']li[data-room='" + room + "']").toggleClass("active");
+                        }
+                    }
+                    
+                    firstAndLastTimeslot[1] = timeslot2;
+                }   
+                else {
+                    //if timeslot is selected before the range
+                    if (firstAndLastTimeslot[0] > timeslot2) {
+                        timeslot = timeslot2;
+                        firstAndLastTimeslot[0] = timeslot2;
+                        for (var i = firstAndLastTimeslot[0]; i <= firstAndLastTimeslot[1] ; i++) {
+
+                            //adds more timeslots to the already active tismeslots
+                            if ($("li[data-timeslot='" + i + "']li[data-room='" + room + "']").hasClass("active")) { }
+                            else {
+                                //toggles active the desired timesots
+                                $("li[data-timeslot='" + i + "']li[data-room='" + room + "']").toggleClass("active");
+                            }
+                        }
+
+
+                        
+                    }
+                    //if timeslot selected is between the range that was already selected
+                    else {
+                        for (var i = parseInt(timeslot2) + 1; i <= parseInt(firstAndLastTimeslot[1]) ; i++) {
+                            $("li[data-timeslot='" + i + "']li[data-room='" + room + "']").toggleClass("active");
+                        }
+                        firstAndLastTimeslot[1] = timeslot2;
+                    }
+
                 }
+                
+
+                //firstAndLastTimeslot[1] = $(this).data("timeslot");
                 $(".reservation-popup h1").html(confirmRservation);
             }
-
+            
         });
     }
 
