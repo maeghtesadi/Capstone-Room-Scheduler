420747a287e525bdb64726268279ec6921f4cbae
limphilip <lim-philip@hotmail.com>
2016-11-09 10:29:14 -0500

    #15 added username to user table
    added username to user table

--- StorageLayer/sql/user_sampledata.sql
+++ StorageLayer/sql/user_sampledata.sql
@@ -1,3 +1,3 @@
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('01','Hannah Ortiz','0',password('hortiz'));
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('02','Ideawin Koun','4',password('ikoun'));
-INSERT INTO `user`(`userID`, `name`, `numOfReservations`, `password`) VALUES ('03','Philip Lim','2',password('plim'));
+INSERT INTO `user` VALUES ('1','h_ortiz',password('hortiz'),'Hannah Ortiz','0');
+INSERT INTO `user` VALUES ('2','i_koun',password('ikoun'),'Ideawin Koun','4');
+INSERT INTO `user` VALUES ('3','p_lim',password('plim'),'Philip Lim','2');
