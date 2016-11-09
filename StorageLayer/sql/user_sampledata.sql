e39d3bb5bb14feba30a923acda614fc7145f3484
limphilip <lim-philip@hotmail.com>
2016-11-09 12:04:09 -0500

    Added admin mock user
    Added admin mock user

--- StorageLayer/sql/user_sampledata.sql
+++ StorageLayer/sql/user_sampledata.sql
@@ -1,3 +1,4 @@
 INSERT INTO `user` VALUES ('1','h_ortiz',password('hortiz'),'Hannah Ortiz','0');
 INSERT INTO `user` VALUES ('2','i_koun',password('ikoun'),'Ideawin Koun','4');
 INSERT INTO `user` VALUES ('3','p_lim',password('plim'),'Philip Lim','2');
+INSERT INTO `user` VALUES ('4','admin',password('admin'),'Administrator','0');
