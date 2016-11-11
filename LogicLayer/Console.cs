cf12219f5aa8c96b1e3f3cc2a1846ab5216c60b1
Nassim <nassimelsayed@hotmail.com>
2016-11-11 15:44:35 -0500

    Added console class and empty methods

---
+++ LogicLayer/Console.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    static class Console
+    {
+
+        static void viewReservations()
+        {
+
+        }
+
+        static void makeReservation()
+        {
+
+        }
+
+        static void modifyReservation()
+        {
+
+        }
+
+        static void cancelReservation()
+        {
+
+        }
+
+        static void viewRoomAvailabilites()
+        {
+
+        }
+
+        static void addToWaitList(int roomID, int timeSlotID, DateTime date, int userID)
+        {
+
+        }
+
+    }
+}
