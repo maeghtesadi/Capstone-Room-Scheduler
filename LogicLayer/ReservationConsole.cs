029483b480793571ae56ed48b3471230f96c60d2
Nassim <nassimelsayed@hotmail.com>
2016-11-11 17:18:41 -0500

    Small changes to start testing backend

---
+++ LogicLayer/ReservationConsole.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace LogicLayer
+{
+    public static class ReservationConsole
+    {
+
+        public static void viewReservations()
+        {
+
+        }
+
+        public static void makeReservation()
+        {
+
+        }
+
+        public static void modifyReservation()
+        {
+
+        }
+
+        public static void cancelReservation()
+        {
+
+        }
+
+        public static void viewRoomAvailabilites()
+        {
+
+        }
+
+        public static void addToWaitList(int roomID, int timeSlotID, DateTime date, int userID)
+        {
+
+        }
+
+    }
+}
