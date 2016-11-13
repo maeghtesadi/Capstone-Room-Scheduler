fbc5f109578aed7732963db187b6d1729b150032
limphilip <lim-philip@hotmail.com>
2016-11-13 15:04:13 -0500

    #34 Initial implementation of room identity map
    - Initial implementation of room identity map
    - Change to RoomMapper accordingly (fix)

---
+++ LogicLayer/RoomIdentityMap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    class RoomIdentityMap
+    {
+        /**
+         * Room Identity Map instance (Singleton)
+         */ 
+        private static RoomIdentityMap instance = new RoomIdentityMap();
+        
+        /**
+         * Dictionary representing the rooms that are currently used in the active memory
+         */ 
+        private Dictionary<int, Room> roomList_ActiveMemory = new Dictionary<int, Room>();
+
+        /**
+         * Private constructor (Singleton)
+         */ 
+        private RoomIdentityMap() { }
+
+        /**
+         * Returns the instance of RoomIdentityMap
+         */ 
+        public static RoomIdentityMap getInstance()
+        {
+            return instance;
+        }
+
+        /**
+         * Adds a room object to the dictionary representing all rooms in the active memory
+         */ 
+        public void addTo(Room room)
+        {
+            roomList_ActiveMemory.Add(room.getRoomID(), room);
+        }
+
+        /**
+         * Removes a room object from the active memory dictionary
+         */ 
+        public void removeFrom(Room room)
+        {
+            roomList_ActiveMemory.Remove(room.getRoomID());
+        }
+
+        /**
+         * Finds and return a room based on its id from the active memory
+         */ 
+        public Room find(int roomID)
+        {
+            Room room;
+            if (roomList_ActiveMemory.TryGetValue(roomID, out room))
+            {
+                return room;
+            }
+            return null;
+        }
+
+        /**
+         * Finds all rooms that are currently in the active memory
+         */
+         public Dictionary<int, Room> findAll()
+        {
+            // Create a new dictionary to be returned
+            Dictionary<int, Room> newDictionary = new Dictionary<int, Room>();
+
+            // Copy each key value pairs (do not need to deep copy the value, room).
+            // We simply want to not return the reference to the dictionary used here.
+            foreach(KeyValuePair<int, Room> pair in this.roomList_ActiveMemory)
+            {
+                newDictionary.Add(pair.Key, pair.Value);
+            }
+
+            return newDictionary;
+        }
+          
+    }
+}
