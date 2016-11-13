594db9fb6415b736fd8082e6b4202e4cb644337a
nim442 <nim442@gmail.com>
2016-11-13 15:30:04 -0800

    folder restructured

--- PresentationLayer/App_Start/BundleConfig.cs
+++ PresentationLayer/App_Start/BundleConfig.cs
@@ -8,31 +8,31 @@ namespace PresentationLayer
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/PresentationLayer/bundles/jquery").Include(
+                        "~/PresentationLayer/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/PresentationLayer/bundles/jqueryval").Include(
+                        "~/PresentationLayer/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/PresentationLayer/bundles/modernizr").Include(
+                        "~/PresentationLayer/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                       "~/Scripts/jquery-ui-1.12.1.js",
-                       "~/Scripts/jquery.unobtrusive-ajax.js",
-                       "~/Scripts/jquery.signalR-2.2.1.js"
+            bundles.Add(new ScriptBundle("~/PresentationLayer/bundles/bootstrap").Include(
+                      "~/PresentationLayer/Scripts/bootstrap.js",
+                      "~/PresentationLayer/Scripts/respond.js",
+                       "~/PresentationLayer/Scripts/jquery-ui-1.12.1.js",
+                       "~/PresentationLayer/Scripts/jquery.unobtrusive-ajax.js",
+                       "~/PresentationLayer/Scripts/jquery.signalR-2.2.1.js"
                        
                       ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-paper.css",
-                      "~/Content/site.css",
-                      "~/Content/dashboard.css",
-                      "~/Content/calendar.css"));
+            bundles.Add(new StyleBundle("~/PresentationLayer/Content/css").Include(
+                      "~/PresentationLayer/Content/bootstrap-paper.css",
+                      "~/PresentationLayer/Content/site.css",
+                      "~/PresentationLayer/Content/dashboard.css",
+                      "~/PresentationLayer/Content/calendar.css"));
         }
     }
 }
--- PresentationLayer/Global.asax.cs
+++ PresentationLayer/Global.asax.cs
@@ -16,6 +16,22 @@ namespace PresentationLayer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Add /MyVeryOwn/ folder to the default location scheme for STANDARD Views
+            var razorEngine = ViewEngines.Engines.OfType<RazorViewEngine>().FirstOrDefault();
+            razorEngine.ViewLocationFormats =
+                razorEngine.ViewLocationFormats.Concat(new string[] {
+            "~/PresentationLayer/Views/{1}/{0}.cshtml",
+            "~/PresentationLayer/Views/{0}.cshtml"
+                    // add other folders here (if any)
+                }).ToArray();
+
+            // Add /MyVeryOwnPartialFolder/ folder to the default location scheme for PARTIAL Views
+            razorEngine.PartialViewLocationFormats =
+                razorEngine.PartialViewLocationFormats.Concat(new string[] {
+            "~/MyVeryOwnPartialFolder/Views/Shared{0}.cshtml"
+                    // add other folders here (if any)
+                }).ToArray();
         }
     }
 }
