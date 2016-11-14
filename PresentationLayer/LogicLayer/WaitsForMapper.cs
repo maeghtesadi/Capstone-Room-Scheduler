5a1a76d1de4c1d5c831a75f7a2f7bbeca36d3425
Ideawin Koun <Ideawin@users.noreply.github.com>
2016-11-13 19:50:04 -0500

    #37 Implementation of the Mapper for WaitsFor
    Contributors:
    Philip Lim
    Hannah Ortiz
    Ideawin Koun

---
+++ PresentationLayer/LogicLayer/WaitsForMapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDG;
+
+namespace Mapper
+{
+    class WaitsForMapper
+    {
+        // Instance of this mapper object
+        private static WaitsForMapper instance = new WaitsForMapper();
+
+        private TDGWaitsFor tdgWaitsFor = TDGWaitsFor.getInstance();
+
+        private WaitsForMapper() { }
+
+        public static WaitsForMapper getInstance()
+        {
+            return instance;
+        }
+
+        public List<int> getAllUsers(int reservationID)
+        {
+            return tdgWaitsFor.getAllUsers(reservationID);
+        }
+
+        public void refreshWaitsFor(List<Reservation> refreshList)
+        {
+            tdgWaitsFor.refreshWaitsFor(refreshList);
+        }
+    }
+}
