57b072f12a87c6f1a5944b49e2d54c389a2da221
Ideawin <ideawinkoun@gmail.com>
2016-11-22 12:47:40 -0500

    Comments in UserCatalog

--- PresentationLayer/LogicLayer/UserCatalog.cs
+++ PresentationLayer/LogicLayer/UserCatalog.cs
@@ -3,37 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Mappers;
 namespace LogicLayer
 {
     public class UserCatalog
     {
-
+        // Singleton
         private static UserCatalog instance = new UserCatalog();
 
+        // List of users with get and set
         public List<User> registeredUsers { get; set; }
 
+        // Constructor
         private UserCatalog()
         {
             registeredUsers = new List<User>();
         }
 
-        public UserCatalog(List<User> regusers)
-        {
-            this.registeredUsers = regusers;
-        }
-
+        // Method to get instance
         public static UserCatalog getInstance()
         {
             return instance;
         }
 
+        // Method to make a new user
         public User makeNewUser(int userID, String username, String password, String name)
         {
             User user = new User(userID, username, password, name);
             return user;
         }
 
+        // Method to modify a user
         public User modifyUser(int userID, String name)
         {
             for(int i = 0; i < registeredUsers.Count; i++)
