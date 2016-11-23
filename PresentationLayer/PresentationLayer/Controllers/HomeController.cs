f15d07f950f006956aaa9b7dffd3c7853fe0aad6
nim442 <nim442@gmail.com>
2016-11-23 10:02:14 -0500

    Mutula exclusisosison

--- PresentationLayer/PresentationLayer/Controllers/HomeController.cs
+++ PresentationLayer/PresentationLayer/Controllers/HomeController.cs
@@ -8,12 +8,24 @@ using Microsoft.AspNet.Identity;
 using LogicLayer;
 using Microsoft.AspNet.SignalR;
 using PresentationLayer.Hubs;
-
+using System.Threading;
 
 namespace CapstoneRoomScheduler.Controllers
 {
+    
     public class HomeController : Controller
     {
+        //Lock object to lock rooms. Eventually add objects to the lockobject dynamically using the total number of rooms
+        //Technically a room should have its own lock object
+        private static object[] _lockobject = new object[] {new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        new object(),new object(),new object(),new object(),new object(),
+        }
+      ;
         public ActionResult Calendar()
         {
             return View();
@@ -22,17 +34,21 @@ namespace CapstoneRoomScheduler.Controllers
         [HttpPost]
         public void makeReservation(int room,string description,int day,int month,int year,int firstTimeSlot, int lastTimeSlot)
         {
-            var userID = Int32.Parse(User.Identity.GetUserId());
-            var date = new DateTime(year, month, day);
-            var weeklyConstraint = ReservationConsole.getInstance().weeklyConstraintCheck(userID, date);
-            var dailyConstraint = ReservationConsole.getInstance().dailyConstraintCheck(userID, date, firstTimeSlot, lastTimeSlot);
-            if (dailyConstraint && weeklyConstraint)
-            {
-                
-                ReservationConsole.getInstance().makeReservation(userID, room, description, date, firstTimeSlot, lastTimeSlot);
-                GlobalHost.ConnectionManager.GetHubContext<CalendarHub>().Clients.Group(User.Identity.GetUserId()).incomingMessage("Reservation has been successfully created");
-                updateCalendar(year, month, day);
+            if(Monitor.TryEnter(_lockobject[room],TimeSpan.FromSeconds(59))){
+                var userID = Int32.Parse(User.Identity.GetUserId());
+                var date = new DateTime(year, month, day);
+                var weeklyConstraint = ReservationConsole.getInstance().weeklyConstraintCheck(userID, date);
+                var dailyConstraint = ReservationConsole.getInstance().dailyConstraintCheck(userID, date, firstTimeSlot, lastTimeSlot);
+                if (dailyConstraint && weeklyConstraint)
+                {
+                  
+                    ReservationConsole.getInstance().makeReservation(userID, room, description, date, firstTimeSlot, lastTimeSlot);
+                    GlobalHost.ConnectionManager.GetHubContext<CalendarHub>().Clients.Group(User.Identity.GetUserId()).incomingMessage("Reservation has been successfully created");
+                    updateCalendar(year, month, day);
+                }
+                Monitor.Exit(_lockobject[room]);
             }
+           
         }
         [HttpPost]
         public void modifyReservation(int roomId,string date,int initialTimeslot,int finalTimeslot,int resid,int day,int month,int year,string description)
@@ -71,12 +87,21 @@ namespace CapstoneRoomScheduler.Controllers
             hubContext.Clients.All.updateWaitlist(ReservationConsole.getInstance().getAllTimeSlots(), convertToJsonObject(ReservationConsole.getInstance().findByDate(date)),ReservationConsole.getInstance().getUserCatalog());
             
             }
-        [LoggedIn]
+        
         [HttpPost]
         public void getReservations() {
-            var hubContext = GlobalHost.ConnectionManager.GetHubContext<CalendarHub>();
-            var JsonListofReservations = convertToJsonObject(ReservationConsole.getInstance().findByUser(Int32.Parse(User.Identity.GetUserId())));
-            hubContext.Clients.All.populateReservations(JsonListofReservations); //returns a list of reservations in the js function
+
+            if (User.Identity.IsAuthenticated) {
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<CalendarHub>();
+                var JsonListofReservations = convertToJsonObject(ReservationConsole.getInstance().findByUser(Int32.Parse(User.Identity.GetUserId())));
+                hubContext.Clients.All.populateReservations(JsonListofReservations); //returns a list of reservations in the js function
+            }
+            else {
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<CalendarHub>();
+                var JsonListofReservations = convertToJsonObject(ReservationConsole.getInstance().getAllReservations());
+                hubContext.Clients.All.populateReservations(JsonListofReservations);
+
+            }
         }
 
         //Techiincally asp/signarl autmatically converts to json when you pass an object to javascript but here we just convert it into an easy to digest object
